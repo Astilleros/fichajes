d127e7d68de206c63baaa48c6b3dd6d4423a0fa0
Astilleros <alejandro.chiva91@gmail.com>
2022-11-29 01:14:05 +0100

    f

--- dist/stripe/stripe.service.js
+++ dist/stripe/stripe.service.js
@@ -36,7 +36,7 @@ let StripeService = class StripeService {
                 },
             ],
             mode: 'payment',
-            payment_method_types: this.cfg.get('PAYMENT_METHOD_TYPES'),
+            payment_method_types: [this.cfg.get('PAYMENT_METHOD_TYPES')],
             success_url: this.cfg.get('DOMAIN'),
             cancel_url: this.cfg.get('DOMAIN'),
         });
